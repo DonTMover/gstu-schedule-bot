4420a51b73e77596ff65b447b0aedd714830ce5b
Egor <dontmover@duck.com>
2025-08-25 22:10:31 +0300

    fixed bug with dict

--- app/api.py
+++ app/api.py
@@ -1,9 +1,21 @@
 import aiohttp
 import asyncio
 from pprint import pprint
-from app.enum import groups
+# from app.enum import groups
 
 BASE_URL = "https://sc.gstu.by/api/schedules/group"
+
+groups = {
+    "АП-11": "ap-11",
+    "АП-21": "ap-21",
+    "АП-31": "ap-31",
+    "АП-12": "ap-41",
+    "АТ-11": "at-11",
+    "АТ-21": "at-21",
+    "АЭ-11": "ae-11",
+    "АЭ-21": "ae-21",
+    "АЭП-11": "aep-11",
+}
 
 async def fetch_schedule(group_name: str) -> dict:
     url = f"{BASE_URL}/{groups[group_name]}"
